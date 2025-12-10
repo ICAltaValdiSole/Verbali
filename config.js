111dc39e695114cf2b1fe9ec9e7a567c16a0a148
I.C.AltaValdiSole <tecnico@icaltavaldisole.it>
2025-12-10 11:16:19 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+/* config.js - DATI ISTITUTO */
+const SCHOOL_CONFIG = {
+    // Intestazione Principale
+    nomeIstituto: "ISTITUTO COMPRENSIVO ALTA VAL DI SOLE",
+
+    // Dettagli Intestazione (indirizzo, contatti, codici)
+    rigaIndirizzo: "Indirizzo: Via S. Michele, 11 | 38026 FUCINE di OSSANA (TN)",
+    rigaContatti: "Tel. 0463 751129 | Fax 0463 750043 | E-mail: segr.icaltavaldisole@scuole.provincia.tn.it",
+    rigaPEC: "PEC: ic.altavaldisole@pec.provincia.tn.it",
+    rigaCodici: "Codice Fiscale/P. IVA: 92013790222 | Codice Ministeriale: TNIC832006",
+
+    // Logo (Assicurati che il file esista nella cartella assets)
+    logoPath: "/assets/logo_scuola.png",
+
+    // Testo piè di pagina
+    footerText: "Verbale generato digitalmente - IC Alta Val di Sole"
+};
+
+
+
+
